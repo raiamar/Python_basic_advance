24b1d114c8ab80730674212121eeccfd5458be83
raiamar <raiamar021@gmail.com>
2021-06-30 14:01:37 +0545

    multilevel & multiple inheritance

--- inheritance.py
+++ inheritance.py
@@ -31,15 +31,85 @@ second_game.a_message()
 second_game.num_of_players()
 '''
 
+'''
 #overloading
 class Product:
-    def __inti__(self, name, price):
+    def __init__(self, name, price):
         self.name = name
-        self.price = price 
+        self.price = price
 
-    #def __eq__(self, object):
-        #return self.price == object.prices
+    def __eq__(self, object):
+        return self.price == object.price
 
-p1 = Product("Pant", 2000)
-p2 = Product("T-shirt", 2000)
-print(p1 == p2)
+    def __add__(self, object):
+        return self.price + self.price
+
+
+pant = Product("Pant", 2000)
+Tshirt = Product("Tshirt", 2000)
+
+print(pant == Tshirt)
+print(pant + Tshirt)
+print(dir(int))
+'''
+
+
+'''
+#Overriding
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def walk(self):
+        print(f"{self.name} is walking in child class")
+
+class Teacher(Person):
+    def __init__(self, name, school):
+        super().__init__(name)
+        self.school = school #adding new attribute
+
+    def walk(self):
+        super().walk() #this code initilize walk function of parent class
+        print(f"{self.name} is walking in parent class")
+
+    def teaching(self):
+        print(f"{self.name} is teaching at {self.school} for 10 years now")
+
+t1 = Teacher("Bob", "xyz")
+t1.walk()
+t1.teaching()
+'''
+
+#Multilevel / multiple inheritance 
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def study(self):
+        print(f"{self.name} is also studing  Physics from person class")
+
+class Tutor(Person):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def Teach(self):
+        print(f"{self.name} also conducts home Tuition")
+
+class Teacher(Person):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def work(self):
+        print(f"{self.name} is {self.age} and work as part-time teacher.")
+
+class Student(Teacher, Tutor): #multiple inheritance
+    def __init__(self,name, age):
+        super().__init__(name, age)
+
+p1 = Student("Bob", 21)
+p1.work()
+p1.study()
+p1.Teach()
